8fc198837f90017f56266014a2252137bc65290a
Syed Hassan Mustafa <Hassan.mustafa@ripeseed.io>
2024-03-06 11:39:11 +0500

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-
-function App() {
+function App({signOut}) {
   return (
     <div >
       <h3>Hello</h3>
+      <button onClick={signOut}>signOut</button>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
